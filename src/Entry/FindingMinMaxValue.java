1fc85b468506f75e9fa988fba477da62c2ffcf9e
gokturkhandogru <gokturkhandogru@gmail.com>
2023-01-17 21:08:44 +0100

    Java101_Course

--- src/Entry/FindingMinMaxValue.java
+++ src/Entry/FindingMinMaxValue.java
@@ -2,21 +2,49 @@ package Entry;
 import java.util.Scanner;
 public class FindingMinMaxValue {
     public static void main(String[] args) {
+
         int x;
-        int max=0, min=0;
+
         Scanner input = new Scanner(System.in);
         System.out.println("How Many Number Do You Want to Write : ");
         x = input.nextInt();
 
+        //calling
+        int numbers[] = add(x);
+        sort(numbers);
+        result(numbers);
+    }
+    static int[] add(int x){
+        int numbers[]=new int[x];
+        Scanner input = new Scanner(System.in);
+        System.out.println("Add is Working");
         for (int i = 1;i<=x;i++){
             System.out.print(i+". Number : ");
             int y = input.nextInt();
-            max = max>y ? max : y;
-            min = min<y ? min : y;
-
+            numbers[i-1] = y;
         }
+        return numbers;
+    }
+    static void sort(int numbers[]){
+        System.out.println("Sort is Working");
+        for (int j = 0; j<numbers.length; j++){
+            int min = numbers[j];
+            int indexMin = j;
 
-        System.out.println(max);
-        System.out.println(x < 0 ? "" : "En küçük sayı: " + min);
+            for (int k= j+1; k<numbers.length; k++){
+                if (numbers[k]<min){
+                    min=numbers[k];
+                    indexMin = k;
+                }
+            }
+            int a = numbers[j];
+            numbers[j] = min;
+            numbers[indexMin] = a;
+        }
+    }
+    static void result(int numbers[]) {
+        System.out.println("Result is Working");
+        System.out.println("Max number is : " + numbers[numbers.length-1]);
+        System.out.println("Min number is : " + numbers[0]);
     }
 }
---
+++ src/Entry/ReverseTriangle.java
@@ -0,0 +1,15 @@
+package Entry;
+import java.util.Scanner;
+public class ReverseTriangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Write a Line Number : ");
+        int x = input.nextInt();
+
+        for (int i = 1; i <= x; i++) {
+            System.out.print("");
+            for (int j = 1; j <= (j * 1) - 1; j++) {
+            }
+        }
+    }
+}
