6702ae2394589d4b2b54d1695df7dd172741db91
gokturkhandogru <gokturkhandogru@gmail.com>
2023-01-17 21:20:08 +0100

    Java101_Course

--- src/Entry/ReverseTriangle.java
+++ src/Entry/ReverseTriangle.java
@@ -7,9 +7,14 @@ public class ReverseTriangle {
         int x = input.nextInt();
 
         for (int i = 1; i <= x; i++) {
-            System.out.print("");
-            for (int j = 1; j <= (j * 1) - 1; j++) {
+            System.out.println("");
+            for (int j = 1; j <= (i * 1) - 1; j++) {
+                System.out.print(" ");
             }
+            for (int k = 1; k<=(2*(x-i)-1); k++){
+                System.out.print("*");
+            }
+
         }
     }
 }
