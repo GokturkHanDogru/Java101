c7f2edf467289183800cd3f097635c24f9ea6a4c
gokturkhandogru <gokturkhandogru@gmail.com>
2023-01-17 22:09:14 +0100

    Java101_Course

---
+++ src/Entry/PrimeNumbers.java
@@ -0,0 +1,21 @@
+package Entry;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+
+            int divisorsNumber = 0;
+
+            for (int i = 1; i <= 100; i++) {
+                for (int j = 1; j <= i; j++) {
+                    if (i % j == 0) {
+                        divisorsNumber++;
+                    }
+                }
+                if (divisorsNumber == 2) {
+                    System.out.print(i + " ");
+                }
+                divisorsNumber = 0;
+            }
+        }
+    }
+
