c773bc69ba64d9bfca901fc93e3b677127e303a2
gokturkhandogru <gokturkhandogru@gmail.com>
2023-01-20 22:09:29 +0100

    Java101_Course

---
+++ src/Entry/Methods.java
@@ -0,0 +1,23 @@
+package Entry;
+
+public class Methods {
+
+    static void helloWorld(){
+        System.out.println("Hello World!");
+    }
+
+   static int power(int base, int exp){
+            int result = 1;
+            for (int i=1; i<=exp; i++){
+                result *=base;
+            }
+
+       return result;
+   }
+    public static void main(String[] args) {
+
+        System.out.println(power(2,3));
+        System.out.println(power(5,6));
+        helloWorld();
+    }
+}
