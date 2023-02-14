ef1ad669ce677e7678bc138dfdeffa0bb2ca5530
gokturkhandogru <gokturkhandogru@gmail.com>
2023-02-14 23:12:50 +0100

    Java101_Course

--- src/Entry/ArraysPractice1.java
+++ src/Entry/ArraysPractice1.java
@@ -10,6 +10,14 @@ public class ArraysPractice1 {
         }
         double average = total/ list.length;
         System.out.println(average);
+
+        int [] list2 = {1,2,3,4,5};
+        double sum=0.0;
+
+        for (int i = 0; i<list2.length; i++){
+            sum+=(1.0/list2[i]);
+        }
+        System.out.println("Harmonica: "+(list2.length/sum));
         }
     }
 
