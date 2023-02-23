0c261e1743300ffa1bcde76a08a4cbf2a9f4a220
gokturkhandogru <gokturkhandogru@gmail.com>
2023-02-23 16:58:31 +0100

    Java101_Course

---
+++ src/Entry/PalindromicWordsWhileLoop.java
@@ -0,0 +1,33 @@
+package Entry;
+
+public class PalindromicWordsWhileLoop {
+
+    static boolean isPalindromic (String str){
+    int i = 0;
+    int j = str.length()-1;
+    while (i<j) {
+        if (str.charAt(i) != str.charAt(j)) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+        return true;
+    }
+
+    static boolean isPalindromic2 (String str){
+        String reverse="";
+
+        for (int i = str.length()-1; i >=0; i--){
+            reverse += str.charAt(i);
+        }
+
+        return str.equals(reverse);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindromic2("Anna"));
+
+    }
+}
